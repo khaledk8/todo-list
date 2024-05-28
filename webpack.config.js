f3f696b2415b87c65c934b6d16fa1da4e9284932
khaledk8 <khalidkari1@gmail.com>
2024-05-28 14:13:37 +0300

    added template for html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
+      template: './src/index.html',
+      filename: 'index.html',
     }),
   ],
   module: {
